1877ebd43158e1ad4d2a7b20075633f9a1207745
AyberkCakar <ayberkcakar35@gmail.com>
2020-09-29 13:35:04 +0300

    feat: added service.

---
+++ src/serve.js
@@ -0,0 +1,19 @@
+
+import path from 'path'
+import express from 'express'
+
+const app = express();
+const routers = require('./routers');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function (req, res) {
+    res.json('Dailyathon Serve Project');
+});
+
+app.use((req, res, next) => {
+    res.send("404 NOT FOUND");
+});
+
+module.exports = app;
