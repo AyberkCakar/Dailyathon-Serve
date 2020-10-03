1bbb099476592c46f206adc334ee8fbd369c776c
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-03 15:37:27 +0300

    feat: added announcement list router

--- src/database/transactions/announcementTransactions.js
+++ src/database/transactions/announcementTransactions.js
@@ -2,6 +2,19 @@ const { mysqlDataContext } = require('../dataContexts');
 const { announcementMessage }  = require('../../fixtures/messageStatus.json');
 
 module.exports = {
+    list: () => {
+        return new Promise((resolve, reject) => {
+            mysqlDataContext.query('SELECT * FROM tblAnnouncement order by AnnouncementID desc', (error, result) => {
+                if (!error)
+                    if (result != null)
+                        resolve(result);
+                    else
+                        reject( announcementMessage.all.Not_Found );
+                else
+                    reject({ status: 500, message: error.message });
+            });
+        });
+    },
     announcementUserList: (UserID) => {
         return new Promise((resolve, reject) => {
             mysqlDataContext.query('SELECT  * FROM tblAnnouncement WHERE AnnouncementID NOT IN (SELECT AnnouncementID FROM tblAnnouncementUser WHERE UserID= ?)', [(UserID)], (error, result) => {
