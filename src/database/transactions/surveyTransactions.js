da16962363a130bb8ff4b6009c29d8306a9a36dc
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-03 15:43:23 +0300

    feat: added survey list router

--- src/database/transactions/surveyTransactions.js
+++ src/database/transactions/surveyTransactions.js
@@ -2,6 +2,19 @@ const { mysqlDataContext } = require('../dataContexts');
 const { surveyMessage }  = require('../../fixtures/messageStatus.json');
 
 module.exports = {
+    list: () => {
+        return new Promise((resolve, reject) => {
+            mysqlDataContext.query('SELECT * FROM tblSurveyList order by SurveyListID desc', (error, result) => {
+                if (!error)
+                    if (result != null)
+                        resolve(result);
+                    else
+                        reject( surveyMessage.all.Not_Found );
+                else
+                    reject({ status: 500, message: error.message });
+            });
+        });
+    },
     surveyUserList: (UserID) => {
         return new Promise((resolve, reject) => {
             mysqlDataContext.query('SELECT  * FROM tblSurveyList WHERE SurveyListID NOT IN (SELECT SurveyListID FROM tblSurveyUser WHERE UserID= ?)', [(UserID)], (error, result) => {
--- src/routers/surveyRouter.js
+++ src/routers/surveyRouter.js
@@ -6,6 +6,15 @@ const router = express();
 const surveyTransactions = dbFactory('surveyTransactions');
 const surveyValidator = validators.surveyValidator;
 
+router.get('/survey', async (req, res) => {
+    try {
+        const response = await surveyTransactions.list();
+        res.json(response);
+    } catch (error) {
+        res.status(error.status).json({ message: error.message });
+    }
+});
+
 router.get('/surveyUserList',verifyToken,surveyValidator.surveyUserList, async (req, res) => {
     try {
         const response = await surveyTransactions.surveyUserList(req.body.UserID);
