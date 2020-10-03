1030522b9f2f6218d6e1aba100434600e2c9f855
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-03 14:42:33 +0300

    feat: added User Survey List router

---
+++ src/database/transactions/surveyTransactions.js
@@ -0,0 +1,19 @@
+const { mysqlDataContext } = require('../dataContexts');
+const { surveyMessage }  = require('../../fixtures/messageStatus.json');
+
+module.exports = {
+    surveyUserList: (UserID) => {
+        return new Promise((resolve, reject) => {
+            mysqlDataContext.query('SELECT  * FROM tblSurveyList WHERE SurveyListID NOT IN (SELECT SurveyListID FROM tblSurveyUser WHERE UserID= ?)', [(UserID)], (error, result) => {
+                console.log(UserID);
+                if (!error)
+                    if (result != null)
+                        resolve(result);
+                    else
+                        reject(surveyMessage.SurveyUserList.Not_Found);
+                else
+                    reject({ status: 500, message: error.message });
+            });
+        });
+    }
+};
