1c2234e31157813626ad327f0804e283bfc696ed
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-03 12:35:35 +0300

    fix: fix auth routing operations

--- src/middleware/validators/authValidator.js
+++ src/middleware/validators/authValidator.js
@@ -1,13 +1,26 @@
-const { validateMessage } = require('../../fixtures/messageStatus.json');
+const { validateMessage,userMessage,adminMessage } = require('../../fixtures/messageStatus.json');
 const joi = require('joi');
 
 module.exports = {
     login: async (req, res, next) => {
         try {
-            await joi.object({
-                UserEmail: joi.string().email().required(),
-                UserPassword: joi.string().max(99).required()
-            }).validateAsync(req.body);
+            switch (req.params.loginTpye) {
+                case 'user':
+                    await joi.object({
+                        UserEmail: joi.string().email().required(),
+                        UserPassword: joi.string().max(99).required()
+                    }).validateAsync(req.body);
+                    break;
+                case 'admin':
+                    await joi.object({
+                        Username: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
+                        Password: joi.string().max(99).required()
+                    }).validateAsync(req.body);
+                    break;
+                default:
+                    res.status(userMessage.signUp.Internal_Server_Error.status).json({message: userMessage.signUp.Internal_Server_Error.message});
+                    return;
+            }
             next();
         } catch (error) {
             res.status(validateMessage.status).send({ message: validateMessage.message });
@@ -16,14 +29,38 @@ module.exports = {
 
     signUp: async (req, res, next) => {
         try {
+            switch (req.params.signupType) {
+                case 'user':
+                    await joi.object({
+                        UserName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
+                        UserSurname:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
+                        UserEmail: joi.string().email().required(),
+                        UserPassword: joi.string().max(99).required(),
+                        UserDate: joi.date().required(),
+                        UserProfession: joi.string().min(2).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
+                        UserCity: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı]+$')).required(),
+                    }).validateAsync(req.body);
+                    break;
+                case 'admin':
+                    await joi.object({
+                        Username: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
+                        Password: joi.string().max(99).required()
+                    }).validateAsync(req.body);
+                    break;
+                default:
+                    res.status(adminMessage.signUp.Internal_Server_Error.status).json({message: adminMessage.signUp.Internal_Server_Error.message});
+                    return;
+            }
+            next();
+        } catch (error) {
+            res.status(validateMessage.status).send({ message: validateMessage.message });
+        }
+    },
+
+    findAdminUsername: async (req, res, next) => {
+        try {
             await joi.object({
-                UserName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
-                UserSurname:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
-                UserEmail: joi.string().email().required(),
-                UserPassword: joi.string().max(99).required(),
-                UserDate: joi.date().required(),
-                UserProfession: joi.string().min(2).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
-                UserCity: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı]+$')).required(),
+                Username: joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
             }).validateAsync(req.body);
             next();
         } catch (error) {
@@ -41,4 +78,4 @@ module.exports = {
             res.status(validateMessage.status).send({ message: validateMessage.message });
         }
     }
-}
+};
