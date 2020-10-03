cb0a2c91a478c52e7b5174410eeae67cafe31a1f
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-03 15:28:17 +0300

    refactor: announcement transactions

--- src/database/transactions/announcementTransactions.js
+++ src/database/transactions/announcementTransactions.js
@@ -2,7 +2,7 @@ const { mysqlDataContext } = require('../dataContexts');
 const { announcementMessage }  = require('../../fixtures/messageStatus.json');
 
 module.exports = {
-    surveyUserList: (UserID) => {
+    announcementUserList: (UserID) => {
         return new Promise((resolve, reject) => {
             mysqlDataContext.query('SELECT  * FROM tblAnnouncement WHERE AnnouncementID NOT IN (SELECT AnnouncementID FROM tblAnnouncementUser WHERE UserID= ?)', [(UserID)], (error, result) => {
                 if (!error)
