deaf3d3f98071b7011b04d322f958db7b965500a
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-03 19:25:37 +0300

    refactor: tag insert sqlQuery

--- src/database/transactions/tagTransactions.js
+++ src/database/transactions/tagTransactions.js
@@ -30,7 +30,7 @@ module.exports = {
     },
     insert: (data) => {
         return new Promise((resolve, reject) => {
-            mysqlDataContext.query('INSERT INTO tblTag SET ?)', [data], (error, result) => {
+            mysqlDataContext.query('INSERT INTO tblTag SET ?', [data], (error, result) => {
                 if (!error)
                     if (result.affectedRows != 0)
                         resolve( TagMessage.insert.Ok );
