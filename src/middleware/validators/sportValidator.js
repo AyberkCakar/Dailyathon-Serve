71603053797d83797bedc5f218dc17272bd53720
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-04 20:57:19 +0300

    fix: fix necessary data for sport

--- src/middleware/validators/sportValidator.js
+++ src/middleware/validators/sportValidator.js
@@ -6,6 +6,7 @@ module.exports = {
         try {
             await joi.object({
                 SportName:joi.string().min(2).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
+                LeagueTableName:joi.string().min(3).pattern(new RegExp('^[A-Za-zÇçÖöŞşÜüĞğİı ]+$')).required(),
             }).validateAsync(req.body);
             next();
         } catch (error) {
