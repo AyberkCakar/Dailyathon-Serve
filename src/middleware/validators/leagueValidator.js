ee877f8e94c4e87df0ff5dfee754992748230c34
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-08 20:44:34 +0300

    feat: added league find router

--- src/middleware/validators/leagueValidator.js
+++ src/middleware/validators/leagueValidator.js
@@ -23,6 +23,16 @@ module.exports = {
             res.status(validateMessage.status).send({ message: validateMessage.message });
         }
     },
+    find: async (req, res, next) => {
+        try {
+            await joi.object({
+                LeagueID:joi.number().min(1).max(99999999999).required()
+            }).validateAsync(req.body);
+            next();
+        } catch (error) {
+            res.status(validateMessage.status).send({ message: validateMessage.message });
+        }
+    },
     add: async (req, res, next) => {
         try {
             await joi.object({
