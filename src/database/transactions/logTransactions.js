01462d9b93abcd2eed7c8c641a3b04f72b5822d0
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-18 14:23:23 +0300

    feat: added admin log insert router

--- src/database/transactions/logTransactions.js
+++ src/database/transactions/logTransactions.js
@@ -2,7 +2,7 @@ const { mysqlDataContext } = require('../dataContexts');
 const { logMessage }  = require('../../fixtures/messageStatus.json');
 
 module.exports = {
-    adminloglist: () => {
+    adminlogList: () => {
         return new Promise((resolve, reject) => {
             mysqlDataContext.query('SELECT * FROM tblAdminLog order by LogID DESC', (error, result) => {
                 if (!error)
@@ -10,6 +10,19 @@ module.exports = {
                         resolve(result);
                     else
                         reject( logMessage.AdminLog.all.Not_Found );
+                else
+                    reject({ status: 500, message: error.message });
+            });
+        });
+    },
+    adminlogInsert: (data) => {
+        return new Promise((resolve, reject) => {
+            mysqlDataContext.query('INSERT tblAdminLog tbl SET ?', [data], (error, result) => {
+                if (!error)
+                    if (result.affectedRows != 0)
+                        resolve( logMessage.AdminLog.insert.Ok );
+                    else
+                        reject(logMessage.AdminLog.insert.Internal_Server_Error);
                 else
                     reject({ status: 500, message: error.message });
             });
--- src/routers/logRouter.js
+++ src/routers/logRouter.js
@@ -4,12 +4,20 @@ const dbFactory = require('../database');
 
 const router = express();
 const logTransactions = dbFactory('logTransactions');
-const logValidator = validators.logValidator;
 
 router.get('/adminlog', async (req, res) => {
     try {
-        const response = await logTransactions.adminloglist();
+        const response = await logTransactions.adminlogList();
         res.json(response);
+    } catch (error) {
+        res.status(error.status).json({ message: error.message });
+    }
+});
+
+router.post('/adminlog',  async (req, res) => {
+    try {
+        const response = await logTransactions.adminlogInsert(req.body);
+        res.json({message:response.message});
     } catch (error) {
         res.status(error.status).json({ message: error.message });
     }
