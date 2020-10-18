4f64c85e60b86c2dd381fd83b118bdc87814081d
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-18 13:50:20 +0300

    fix: fix announcement validator

--- src/middleware/validators/announcementValidator.js
+++ src/middleware/validators/announcementValidator.js
@@ -29,7 +29,7 @@ module.exports = {
                 AnnouncementTitle:joi.string().min(2).required(),
                 AnnouncementContent:joi.string().min(2).required(),
                 AnnouncementDate:joi.date().required(),
-                Visible: required()
+                Visible: joi.required()
             }).validateAsync(req.body);
             next();
         } catch (error) {
