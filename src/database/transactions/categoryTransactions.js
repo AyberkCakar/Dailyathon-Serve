67686c3c44328451da2d64fa8350da002b18def7
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-23 00:28:33 +0300

    fix: category find transactions

--- src/database/transactions/categoryTransactions.js
+++ src/database/transactions/categoryTransactions.js
@@ -19,8 +19,8 @@ module.exports = {
         return new Promise((resolve, reject) => {
             mysqlDataContext.query('SELECT * FROM tblCategory WHERE CategoryID = ?',[CategoryID], (error, result) => {
                 if (!error)
-                    if (result != null)
-                        resolve(result);
+                    if (result[0] != null)
+                        resolve(result[0]);
                     else
                         reject( categoryMessage.all.Not_Found );
                 else
