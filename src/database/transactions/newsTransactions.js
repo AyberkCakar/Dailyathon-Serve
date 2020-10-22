cfb4cd63aa7ccfe79ef264d86a33e540c316d51b
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-22 13:26:03 +0300

    fix: news category list delete

--- src/database/transactions/newsTransactions.js
+++ src/database/transactions/newsTransactions.js
@@ -2,28 +2,12 @@ const { mysqlDataContext } = require('../dataContexts');
 const { newsMessage }  = require('../../fixtures/messageStatus.json');
 
 module.exports = {
-    newsCategoryList: () => {
-        return new Promise((resolve, reject) => {
-            mysqlDataContext.query('SELECT * FROM tblNewsCategory order by NewsCategoryID asc', (error, result) => {
-                if (!error)
-                    if (result != null)
-                        resolve(result);
-                    else
-                        reject( newsMessage.newsCategoryList.Not_Found );
-                else
-                    reject({ status: 500, message: error.message });
-            });
-        });
-    },
     list: () => {
         return new Promise((resolve, reject) => {
-            mysqlDataContext.query('SELECT NewsID,NewsTitle,NewsDescription,C.NewsCategoryID,NewsCategoryName FROM ' +
-                'tblNews N inner join tblnewscategory C\n' +
-                '    on N.NewsCategoryID = C.NewsCategoryID\n' +
-                'order by NewsID desc', (error, result) => {
+            mysqlDataContext.query('CALL NewsList()', (error, result) => {
                 if (!error)
-                    if (result != null)
-                        resolve(result);
+                    if (result[0] != null)
+                        resolve(result[0]);
                     else
                         reject( newsMessage.all.Not_Found );
                 else
--- src/routers/newsRouter.js
+++ src/routers/newsRouter.js
@@ -6,15 +6,6 @@ const router = express();
 const newsTransactions = dbFactory('newsTransactions');
 const newsValidator = validators.newsValidator;
 
-router.get('/news-category', async (req, res) => {
-    try {
-        const response = await newsTransactions.newsCategoryList();
-        res.json(response);
-    } catch (error) {
-        res.status(error.status).json({ message: error.message });
-    }
-});
-
 router.get('/news', async (req, res) => {
     try {
         const response = await newsTransactions.list();
