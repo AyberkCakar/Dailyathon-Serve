10aff0d67868e1cdbb9ba1583368d4892a2e90ba
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-26 17:02:51 +0300

    refactor: tag select validator

--- src/middleware/validators/tagValidator.js
+++ src/middleware/validators/tagValidator.js
@@ -33,18 +33,6 @@ module.exports = {
             res.status(validateMessage.status).send({ message: validateMessage.message });
         }
     },
-    tagSelect: async (req, res, next) => {
-        try {
-            await joi.object({
-                UserID:joi.number().required(),
-                TagName:joi.string().required(),
-                RegDate: joi.required()
-            }).validateAsync(req.body);
-            next();
-        } catch (error) {
-            res.status(validateMessage.status).send({ message: validateMessage.message });
-        }
-    },
     update: async (req, res, next) => {
         try {
             await joi.object({
