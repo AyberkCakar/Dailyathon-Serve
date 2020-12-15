41272735c171b7ae70b1d2b43982af03791e30d7
AyberkCakar <ayberkcakar35@gmail.com>
2020-12-15 17:06:00 +0300

    feat: added news router logger

--- src/routers/newsRouter.js
+++ src/routers/newsRouter.js
@@ -4,14 +4,18 @@ const dbFactory = require('../database');
 
 const router = express();
 const newsTransactions = dbFactory('newsTransactions');
+const logTransactions = dbFactory('logTransactions');
 const newsValidator = validators.newsValidator;
 
+const date = new Date();
 router.get('/news', verifyToken, async (req, res) => {
     try {
         const response = await newsTransactions.list();
         res.json(response);
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,null,response.message,date);
     } catch (error) {
         res.status(error.status).json({ message: error.message });
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,error.message,response.message,date);
     }
 });
 
@@ -19,8 +23,10 @@ router.post('/user-news',verifyToken,newsValidator.userNews, async (req, res) =>
     try {
         const response = await newsTransactions.userNewsList(req.body);
         res.json(response);
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,null,response.message,date);
     } catch (error) {
         res.status(error.status).json({ message: error.message });
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,error.message,response.message,date);
     }
 });
 
@@ -28,8 +34,10 @@ router.post('/news-find',verifyToken,newsValidator.find, async (req, res) => {
     try {
         const response = await newsTransactions.find(req.body.NewsID);
         res.json(response);
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,null,response.message,date);
     } catch (error) {
         res.status(error.status).json({ message: error.message });
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,error.message,response.message,date);
     }
 });
 
@@ -37,8 +45,10 @@ router.post('/news', async (req, res) => {
     try {
         const response = await newsTransactions.insert(req.body);
         res.json({message:response.message});
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,null,response.message,date);
     } catch (error) {
         res.status(error.status).json({ message: error.message });
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,error.message,response.message,date);
     }
 });
 
@@ -46,8 +56,10 @@ router.delete('/news', verifyToken, newsValidator.delete, async (req, res) => {
     try {
         const response = await newsTransactions.delete(req.body.NewsID);
         res.json({message:response.message});
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,null,response.message,date);
     } catch (error) {
         res.status(error.status).json({ message: error.message });
+        await logTransactions.servelogInsert(req.originalUrl,req.method,res.statusCode,res.statusMessage,error.message,response.message,date);
     }
 });
 module.exports = router;
